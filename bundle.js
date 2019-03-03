4715a9e27bc404d8f578bb819da61469fe558dcb
Duan-Yi <1370142145@qq.com>
2019-03-03 21:30:36 +0800

    w

--- bundle.js
+++ bundle.js
@@ -35431,7 +35431,8 @@
                 }
             }
         });
-        _vue2.default.http.options.root = 'http://www.liulongbin.top:3005';
+        // http:
+        _vue2.default.http.options.root = 'www.liulongbin.top:3005';
         _vue2.default.http.options.emulateJSON = true;
 
         _vue2.default.filter('dateForm', function(dataStr) {
